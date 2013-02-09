245f5dc504e5c862259046ec78d1e051bb759dba
David DeSandro <david@desandro.com>
2013-02-09 10:06:00 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# docListener
+
+Cross browser DOMContentLoaded event emitter. Supported by IE8+ and good browsers.
+
+Basically a bower-component-ized version of [dperini/ContentLoaded](https://github.com/dperini/ContentLoaded)
+
+```js
+docListener.on( 'ready', function() {
+  // do stuff...
+});
+```
+
+## Bower
+
+docListener is a [Bower](https://github.com/twitter/bower) component
+
+``` bash
+bower install desandro/doc-listener
+```
---
+++ doc-listener.js
@@ -0,0 +1,38 @@
+/*!
+ * docListener
+ * Cross browser DOMContentLoaded event emitter
+ */
+
+/*jshint browser: true, strict: true, undef: true, unused: true*/
+
+( function( window ) {
+
+'use strict';
+
+var EventEmitter = window.EventEmitter;
+var eventie = window.eventie;
+
+var document = window.document;
+
+var docListener = new EventEmitter();
+
+var done = false;
+
+function init( event ) {
+  // bail if IE8 document is not ready just yet
+  var isIE8NotReady = event.type === 'readystatechange' && document.readyState !== 'complete';
+  if ( done || isIE8NotReady ) {
+    return;
+  }
+  done = true;
+  docListener.emit( 'ready', event );
+}
+
+eventie.bind( document, 'DOMContentLoaded', init );
+eventie.bind( document, 'readystatechange', init );
+eventie.bind( window, 'load', init );
+
+// transport
+window.docListener = docListener;
+
+})( this );
